97155e99d7c0d69012ec4ae929bd19e1ec57a81f
Yann Vanhalewyn <yann_vanhalewyn@hotmail.com>
2015-11-07 15:49:51 +0100

    Creating a new user in the DB

--- src/controllers/users_controller.js
+++ src/controllers/users_controller.js
@@ -2,10 +2,26 @@
 
   "use strict";
 
+  var User = require('../models/user')
+    , bcrypt = require('../helpers/bcrypt-promisified')
+
   var users_controller = {
 
     create: function(req, res) {
-      res.send(req.body);
+
+      bcrypt.hash(req.body.password, 10).then(function(hash) {
+
+        var newUser = new User({
+            first_name: req.body.first_name,
+            last_name: req.body.last_name,
+            email: req.body.email,
+            password_digest: hash
+        });
+
+        newUser.save();
+
+        res.send(req.body);
+      });
     }
   }
 
