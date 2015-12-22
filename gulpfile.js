d37c2d30fc255e15f3b063bb9e2e5f47ee1fde3e
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2015-12-22 22:56:39 +0100

    Add sticky footer and choice of header
    the header can be either the homepage header or premium header

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp')
   , sass = require('gulp-sass')
   , rename = require('gulp-rename')
+  , livereload = require('gulp-livereload')
 
 gulp.task('sass', function() {
   gulp.src('./app/sass/index.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./public/css'))
+    .pipe(livereload())
 });
 
 gulp.task('sass:form', function() {
@@ -15,6 +17,7 @@ gulp.task('sass:form', function() {
 });
 
 gulp.task('sass:watch', function() {
+  livereload.listen();
   gulp.watch("./app/sass/**/*.scss", ['sass']);
 });
 
