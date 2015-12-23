920f8d24b8d8e5b811dcde0cf8ec5609c9e4dbf6
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2015-12-23 20:52:37 +0100

    Majorly refactor requiring of vendor libs.
    Wrote a gulp task to concat a bunch of script tags together. Most vendor scripts
    are to be bundled for the homepage. Update de header_premium to also make use of
    boostrap nice navbar collapsing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
-var gulp = require('gulp')
-  , sass = require('gulp-sass')
-  , rename = require('gulp-rename')
+var gulp       = require('gulp')
+  , sass       = require('gulp-sass')
+  , rename     = require('gulp-rename')
   , livereload = require('gulp-livereload')
+  , concat     = require('gulp-concat')
+  , uglify     = require('gulp-uglify')
 
 gulp.task('sass', function() {
   gulp.src('./app/sass/index.scss')
@@ -15,5 +17,28 @@ gulp.task('sass:watch', function() {
   gulp.watch("./app/sass/**/*.scss", ['sass']);
 });
 
+var jsFiles = [
+  './app/js/vendor/jquery.easing.min.js',
+  './app/js/vendor/modernizr.custom.js',
+  './app/js/vendor/classie.min.js',
+  './app/js/vendor/uiMorphingButton_fixed.js',
+  './app/js/progressbar.js',
+  './app/js/validateSignupForm.js',
+  './app/js/morphbuttonsNoscroll.js'
+]
+
+gulp.task('js', function() {
+  return gulp.src(jsFiles)
+    .pipe(concat('bundle.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/js'))
+    .pipe(livereload())
+});
+
+gulp.task('js:watch', function() {
+  livereload.listen();
+  return gulp.watch(jsFiles, ['js'])
+})
+
 gulp.task('default', ['sass']);
-gulp.task('build:production', ['sass'])
+gulp.task('build:production', ['sass', 'js'])
