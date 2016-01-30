eacd47520e8b8e5297ead081b419836ac4b1b13b
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-01-30 15:26:24 +0100

    Add spinner to play icon next to title when loading song

--- app/js/premium/audioplayer/song.jsx
+++ app/js/premium/audioplayer/song.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 export default class Song extends React.Component {
   render() {
     var classes = `track ${this.props.active ? "active" : ""}`
+    var icon = this.props.loading ? 'img/spinner.svg' : 'img/musicplay.png'
+    var style = { width: this.props.loading ? '13px' : '10px' }
 
     return (
       <li className={classes} onClick={this._onClick.bind(this)}>
-        <img className='playicon' src='img/musicplay.png' alt='Play Icon' />
+        <img className='playicon' src={icon} alt='Play Icon' style={style} />
         <h4 className='title'>{this.props.title}</h4>
         <p className='subtitle'>{this.props.subtitle}</p>
         <div className='trackdevide'></div>
