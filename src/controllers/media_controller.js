f622b843c231e9a8eece3bc0df04abefee43c475
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-01 15:12:35 +0100

    Lazily fetch img_ids from cloudinary in media_controller

--- src/controllers/media_controller.js
+++ src/controllers/media_controller.js
@@ -3,13 +3,25 @@ var express = require('express')
   , requireVerifiedEmail = require('../middlewares/require_verified_email')
   , paywall = require('../middlewares/paywall')
   , handleUnpaidUser = require('../middlewares/handle_unpaid_user')
+  , cloudinary = require('../lib/cloudinary')
+
+var img_ids;
 
 var VideosController = {
   middlewares: {
     index: [requireLogin, requireVerifiedEmail, paywall, handleUnpaidUser]
   },
 
-  index: (req, res) => res.render('premium')
+  index: (req, res) => {
+    if (!img_ids) {
+      cloudinary.fetchIdsFor('entry_1').then(ids => {
+        img_ids = ids
+        res.render('premium', { images: img_ids })
+      })
+    } else {
+      return res.render('premium', { images: img_ids })
+    }
+  }
 }
 
 module.exports = VideosController;
