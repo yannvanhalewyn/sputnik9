73349c9ad36ce6a4cb7d9576e16c46f6395edb03
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-01 16:36:34 +0100

    Add fetchIdsForMultiple cloudinary helper method

--- src/lib/cloudinary.js
+++ src/lib/cloudinary.js
@@ -1,6 +1,7 @@
 var cloudinary = require('cloudinary')
   , config     = require('../../config/config')
   , _          = require("lodash")
+  , Q          = require('q')
 
 cloudinary.config({
   cloud_name: 'sputnik9',
@@ -8,7 +9,15 @@ cloudinary.config({
   api_secret: config.cloudinary_api_secret
 })
 
-module.exports.fetchIdsFor = directory => {
+var fetchIdsFor = directory => {
   return cloudinary.v2.api.resources({prefix: directory, type: 'upload'})
     .then(result => _.map(result.resources, r => r.public_id))
 }
+
+var fetchIdsForMultiple = directories => {
+  return Q.all(_.map(directories, fetchIdsFor))
+    .then(results => _.mapKeys(results, (_, idx) => directories[idx]))
+}
+
+module.exports.fetchIdsFor = fetchIdsFor;
+module.exports.fetchIdsForMultiple = fetchIdsForMultiple;
