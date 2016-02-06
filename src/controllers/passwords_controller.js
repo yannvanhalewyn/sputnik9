7dcfd9287d496821c3a6c7842934fdbb30bb29e7
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-06 16:35:05 +0100

    Add controller action for requesting new password reset

--- src/controllers/passwords_controller.js
+++ src/controllers/passwords_controller.js
@@ -1,10 +1,23 @@
 'user strict';
 
+var User = require('../models/user')
+  , password = require('../lib/password')
+  , emails = require('../helpers/emails')
+  , mailer = require('../lib/mailer')
+
 module.exports = {
 
   // POST /users/forgot
-  forgot(req, res) {
-
+  forgot(req, res, next) {
+    User.findOne({email: req.body.email}).then(user => {
+      if (!user) return next(`Geen gebruiker gevonden met email ${req.body.email}`)
+      return password.setResetToken(user).then(user => {
+        return emails.password_reset(user).then(mailer.send).then(() => {
+          req.session.flash = { type: 'success', message: 'Wachtwoord reset email is verstuurd!' }
+          res.redirect('/')
+        })
+      })
+    }, next)
   },
 
   // GET /users/reset/:token
