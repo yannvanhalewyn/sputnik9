e75ce505b76ffe8147e552e55fb1e2621aa8a83e
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-06 17:20:30 +0100

    Wrote POST reset/:token controller method

--- src/controllers/passwords_controller.js
+++ src/controllers/passwords_controller.js
@@ -5,6 +5,13 @@ var User = require('../models/user')
   , emails = require('../helpers/emails')
   , mailer = require('../lib/mailer')
 
+var getUserByResetToken = (token) => {
+  return User.findOne({
+    "local_data.password_reset_token": token,
+    "local_data.password_reset_expiration": { $gt: Date.now() }
+  })
+}
+
 module.exports = {
 
   // POST /users/forgot
@@ -22,10 +29,7 @@ module.exports = {
 
   // GET /users/reset/:token
   show_reset_password(req, res, next) {
-    User.findOne({
-      "local_data.password_reset_token": req.params.token,
-      "local_data.password_reset_expiration": { $gt: Date.now() }
-    }).then(user => {
+    getUserByResetToken(req.params.token).then(user => {
       if (!user) {
         req.session.flash = ({ type: 'error', message: 'Deze wachtwoord reset pagina bestaat niet of is expired' })
         return res.redirect('/')
@@ -35,7 +39,22 @@ module.exports = {
   },
 
   // POST /users/reset/:token
-  reset_password(req, res) {
-
+  reset_password(req, res, next) {
+    getUserByResetToken(req.params.token).then(user => {
+      if (!user) {
+        req.session.flash = ({
+          type: 'error',
+          message: 'Deze wachtwoord reset pagina bestaat niet of is expired'
+        })
+        return res.redirect('/')
+      }
+      user.resetPassword(req.body.password).then(u => {
+        req.session.flash = ({
+          type: 'success',
+          message: 'Je wacthwoord is gereset. Je kan nu inloggen met je nieuwe wachtwoord'
+        })
+        res.redirect('/')
+      })
+    }, next)
   }
 }
