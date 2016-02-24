01c9955f193615d95ec76166cdb4196068a6adb6
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-24 20:05:21 +0100

    Remove unused handlebars cloudinary helpers

--- src/helpers/handlebars.js
+++ src/helpers/handlebars.js
@@ -2,23 +2,9 @@
 
   "use strict";
 
-  var cloudinary = require('cloudinary');
-
-  var cl_transformations = {
-    lightbox_full: { width: 1000, crop: 'scale', secure: true },
-    lightbox_thumb_tag: {
-      width: 400, height: 400, crop: 'thumb',
-      gravity: 'center', class: 'img-responsive', secure: true
-    }
-  }
-
   var helpers = {
     active_view: (a, b) => a == b ? 'active' : '',
-    checkmark: (val) => val ? '&#10003;' : '',
-
-    cl_lightbox_url: (id) => cloudinary.url(id, cl_transformations.lightbox_full),
-    cl_thumb_tag: (id) => cloudinary.image(id, cl_transformations.lightbox_thumb_tag)
-
+    checkmark: (val) => val ? '&#10003;' : ''
   }
 
   module.exports = helpers;
