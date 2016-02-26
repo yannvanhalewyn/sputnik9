447363e4bae3f42274ab2a4670a8a4b27d94519d
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-26 20:41:44 +0100

    Accept extended url-encoded forms (nested hashes)

--- app.js
+++ app.js
@@ -22,7 +22,7 @@
   app.use(express.static('public'));
 
   // Body parser
-  app.use(bodyParser.urlencoded({extended: false}));
+  app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
 
   // Session and persistent session
