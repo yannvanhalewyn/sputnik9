72ab72c6a5e0c68ef54cf2b3401178242c1ed8b1
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-26 19:16:20 +0100

    Add redirection middleware and display admin flash messages

--- src/controllers/admin/entries_controller.js
+++ src/controllers/admin/entries_controller.js
@@ -2,12 +2,18 @@
 
 var requireAdmin = require('../../middlewares/require_admin')
   , Entry = require('../../models/entry')
+  , findEntry = require('../../middlewares/find_entry')
+
+var redirect = (url) => (err, req, res, next) => {
+  req.session.flash = { type: 'danger', message: err }
+  res.redirect(url)
+}
 
 var AdminEntriesController = {
   middlewares: {
     index: [requireAdmin],
-    edit: [requireAdmin],
-    update: [requireAdmin]
+    edit: [requireAdmin, findEntry, redirect('/admin/entries')],
+    update: [requireAdmin, findEntry, redirect('/admin/entries')]
   },
 
   index: (req, res) => {
@@ -17,7 +23,7 @@ var AdminEntriesController = {
   },
 
   edit: (req, res) => {
-    res.render('admin/entries/edit', {layout: 'admin'})
+    res.render('admin/entries/edit', {layout: 'admin', entry: req.entry})
   },
 
   update: (req, res) => {
--- src/routers/admin_entries_router.js
+++ src/routers/admin_entries_router.js
@@ -2,7 +2,7 @@ var router = require('express').Router()
   , controller = require('../controllers/admin/entries_controller')
 
 router.get('/', controller.middlewares.index, controller.index)
-router.get('/:id', controller.middlewares.edit, controller.edit)
-router.post('/:id', controller.middlewares.update, controller.update)
+router.get('/:entry_id', controller.middlewares.edit, controller.edit)
+router.post('/:entry_id', controller.middlewares.update, controller.update)
 
 module.exports = router;
