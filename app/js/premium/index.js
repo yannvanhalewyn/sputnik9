24b2cfc329c2b86469ed7bf966e1b459bb9bc978
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-26 18:12:50 +0100

    Use json entry-data on front-end

--- app/js/premium/index.js
+++ app/js/premium/index.js
@@ -4,17 +4,19 @@ import PremiumPage from './premium_page.jsx';
 import Entry from './entry/entry.jsx';
 import * as resize from './resize';
 
-render(
-  <Router>
-    <Route path="/" component={PremiumPage} foo={"bar"}>
-      <IndexRedirect to="/entries/1" />
-      <Route path="/entries/:entryId" component={Entry} />
-    </Route>
-    <Redirect from="*" to="/" />
-  </Router>,
-  document.getElementById('premium_page')
-)
+$(document).ready(() => {
+  render(
+    <Router>
+      <Route path="/" component={PremiumPage} >
+        <IndexRedirect to="/entries/1" />
+        <Route path="/entries/:entryId" component={Entry} />
+      </Route>
+      <Redirect from="*" to="/" />
+    </Router>,
+    document.getElementById('premium_page')
+  )
 
-$(window).on('resize', resize.video)
-         .on('resize', resize.tracklist)
-         .resize();
+  $(window).on('resize', resize.video)
+           .on('resize', resize.tracklist)
+           .resize();
+})
