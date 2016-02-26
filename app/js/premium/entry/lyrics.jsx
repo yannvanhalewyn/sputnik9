8296bcfdabae7d5c826dd0761e7f01ec7b85fc05
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-27 00:27:17 +0100

    Apply markdown parsing to lyrics

--- app/js/premium/entry/lyrics.jsx
+++ app/js/premium/entry/lyrics.jsx
@@ -1,30 +1,37 @@
 import React from 'react'
+import * as Markdown from '../util/markdown'
+
+class Column extends React.Component {
+  render() {
+    return <div>
+      {this.props.statements.map(this._renderStatement)}
+    </div>
+  }
+
+  _renderStatement(statement, i) {
+    switch(statement.type) {
+      case 'header':
+        return <span className="md-title">{statement.value}<br/></span>
+      default:
+        return <span>{statement.value}<br/></span>
+    }
+  }
+}
 
 export default class Lyrics extends React.Component {
   render() {
-    var columns = this.props.lyrics.split('\n\n\n')
+    var tree = Markdown.parse(this.props.lyrics)
+    let grid = `col-md-${12/tree.length}`
+
     return <div className="lyrics">
       <h3 className="title">Lyrics</h3>
       <div className="body row">
-        {columns.map(this._renderColumn.bind(this))}
-        <div className="col-md-6">
-        </div>
+        {tree.map((column, i) => {
+          return <div className={grid}>
+            <Column key={i} statements={column} />
+          </div>
+        })}
       </div>
     </div>
-  }
-
-  _renderColumn(column, i) {
-    var paragraphs = column.split('\n\n')
-    return <div key={i} className="col-md-6">
-      {paragraphs.map(this._renderParagraph)}
-    </div>
-  }
-
-  _renderParagraph(paragraph, i) {
-    var sentences = paragraph.split('\n')
-    return <span key={i}>
-      {sentences.map((s, i) => <span key={i}>{s}<br/></span>)}
-      <br/>
-    </span>
   }
 }
--- app/js/premium/util/markdown.js
+++ app/js/premium/util/markdown.js
@@ -1,10 +1,13 @@
 export function parse(md) {
-  var title_rx = /^# \w+/
+  var title_rx = /^# (.+)/
   var column_rx = /--+/
   return md.split(column_rx).map(columns => {
     return columns.trim().split('\r\n').map(value => {
       var type = 'text';
-      if (title_rx.test(value)) type = 'header'
+      if (title_rx.test(value)) {
+        type = 'header'
+        value = title_rx.exec(value)[1]
+      }
       return {type, value}
     })
   })
