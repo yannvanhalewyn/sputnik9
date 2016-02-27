9f59adc651d6ebfa919d0ba89303590d2a6243ce
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-28 00:12:14 +0100

    Setup songsForm react component

--- app/js/admin/songs_form/components/songs_form.jsx
+++ app/js/admin/songs_form/components/songs_form.jsx
@@ -1,7 +1,29 @@
 import React from 'react'
+import { FieldSet, TextArea, TextInput } from '../../util/form_elements.jsx'
 
-export default class SongsForm extends React.Component {
+class SongsForm extends React.Component {
   render() {
-    return <div>Foo bar</div>
+    return <form method='post'>
+      {this.props.songs.map(this._renderSong)}
+      <input className='btn btn-success' type="submit" value="Opslaan"/>
+    </form>
+  }
+
+  _renderSong(song) {
+    return <div key={song._id}>{song.title}</div>
+  }
+}
+
+export default class SongsFormContainer extends React.Component {
+  constructor(props) {
+    super()
+    this.state = { songs: props.songs }
+  }
+
+  render() {
+    return <div>
+      <h3>Songs</h3>
+      <SongsForm songs={this.state.songs} />
+    </div>
   }
 }
