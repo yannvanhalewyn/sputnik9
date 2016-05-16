225cd41d08482546353695879747f0329367a32e
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-05-16 16:20:12 +0200

    Add handle to current entry for the header

--- app/js/premium/entry/header.jsx
+++ app/js/premium/entry/header.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import { Link } from 'react-router';
+import { Link, Router } from 'react-router';
+
+// My apologies for the hack, out of time
+var current_entry_idx = (pathname) => {
+  let match = pathname.match("entries/([0-9])*");
+  if (match) return match[1]
+}
 
 class EntryTab extends React.Component {
   render() {
@@ -15,9 +21,16 @@ class EntryTab extends React.Component {
 
 export default class Header extends React.Component {
   render() {
-    return <ul className="entries-header">
-      {this.props.entries.map(this._renderEntryLink.bind(this))}
-    </ul>
+    let entry_idx = current_entry_idx(this.props.location.pathname)
+    let current_entry = this.props.entries[entry_idx || 0]
+    return (
+      <div className="btn-group">
+        <button className="dropdown-toggle current-entry__title" data-toggle="dropdown">{current_entry.title}</button>
+        <ul className="entries-dropdown dropdown-menu">
+          {this.props.entries.map(this._renderEntryLink.bind(this))}
+        </ul>
+      </div>
+    )
   }
 
   _renderEntryLink(entry, i) {
